9419ca700338e695dc78472bf002c537a0d119e5
Exponefrv1 <nastya.vishevskaya@gmail.com>
2023-08-10 20:33:33 +0300

    Update tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -7,5 +7,5 @@ async def test_auth():
     login = "8613402562575"
     password = "cmkkKFGt58aW"
     async with AsyncVKAuth(login = login, password = password) as auth:
-        cookies = await auth.get_auth_cookies() 
+        cookies = await auth.get_auth_cookies()
         assert cookies == "id=816380914; first_name=Shantel; photo_50=https%3A%2F%2Fvk.com%2Fimages%2Fcamera_50.png"
--- tests/test_search.py
+++ tests/test_search.py
@@ -9,4 +9,4 @@ async def test_search():
         search_results = await music.search(query = "Imagine Dragons - Bones")
         title, download_link = next(iter(search_results.items()))
         assert title == "Imagine Dragons - Bones"
-        assert download_link == "/download.php?id=32350711_456239163;35_43a58a32cf9121093e_09a88cef1c4bb81fb3&hash=f208cd6809ec0a6c42625495b9ae032b8809b70485a3c480ccdd0e629ab42e31"
+        assert "download.php" in download_link
